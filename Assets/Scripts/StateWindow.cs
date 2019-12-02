e2865e7d864d14ccfb6264c679071c34ea73c259
game_dept <glasses001222@gmail.com>
2019-12-02 14:51:45 +0900

    up

--- Assets/Scripts/StateWindow.cs
+++ Assets/Scripts/StateWindow.cs
@@ -1,29 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StateWindow : MonoBehaviour, IWindow
 {
+    [SerializeField]
+    Text hpText;
+    [SerializeField]
+    Text defText;
     // Start is called before the first frame update
+
     void Start()
     {
-        
+        PlayerDataManager.Instance.updatestate += UIUpdate;
     }
 
     public void ShowWindow()
     {
         gameObject.SetActive(true);
+        UIUpdate(PlayerDataManager.Instance.GetPlayerData());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void HideWindow()
     {
         gameObject.SetActive(false);
         Time.timeScale = 1;
+    }
+
+    void UIUpdate(PlayerDataManager.PlayerData param)
+    {
+        hpText.text = string.Format("{0}", param.maxhp);
+        defText.text = string.Format("{0}", param.def);
     }
 }
